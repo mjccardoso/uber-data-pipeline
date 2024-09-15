02a0f5b7db90aa56352b9c10d7bc3fa709cd4e23
mauurao <geralxk@gmail.com>
2024-09-15 01:17:43 +0100

    Etl extract_locally -> add new logging with refinement of request body. add sample data for each new extraction

--- src/common_lib.py
+++ src/common_lib.py
@@ -2,3 +2,19 @@ import pandas
 import pyarrow.parquet as pq
 import requests
 import io
+import os
+from pathlib import Path
+import logging
+
+root_marker = 'README.md'
+current_dir = Path.cwd()
+while not (current_dir / root_marker).exists() and current_dir != current_dir.parent:
+    current_dir = current_dir.parent
+
+os.makedirs(f"{current_dir}/logs/", exist_ok=True)
+
+logging.basicConfig(
+    filename=f"{current_dir}/logs/extraction_{pandas.Timestamp.now()}.log",
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
--- src/etl_pipeline.py
+++ src/etl_pipeline.py
@@ -5,36 +5,49 @@ class etl:
     def __init__(self) -> None:
         pass
     
-    def extract(self, type:str, year:str, month:str):
-        """_summary_
+    def extract_locally(self, type:str, year:str, month:str):
+        """
+        Extracts the data from a Parquet file from the URL and transforms it into a Pandas DataFrame.
 
         Args:
-            type (str): _description_
-            year (str): _description_
-            month (str): _description_
+            type (str): Type of data to be extracted (e.g. ‘yellow_tripdata’).
+            year (str): Year of the data.
+            month (str): Month of the data.
 
         Returns:
-            _type_: _description_
+            pd.DataFrame: DataFrame with the extracted data or None if the extraction fails.
         """
         url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/{type}_{year}-{month}.parquet'
-        request = requests.get(url)
+        logging.info(f'Extraction Begging {url} {pandas.Timestamp.now()}')
         
-        if request.status_code == 200:    
-            parquet_buffer = io.BytesIO(x.content)
-            table = pq.read_table(parquet_buffer)    
-            return table.to_pandas()
-        else:
-            print(f'Fail to obtain data: {request.status_code}')
+        try:
+            request = requests.get(url=url)
+            
+            if request.status_code == 200:   
+                
+                parquet_buffer = io.BytesIO(initial_bytes=request.content)
+                table = pq.read_table(source=parquet_buffer)
+                dataframe = pandas.DataFrame()      
+                dataframe = table.to_pandas()
+                dataframe_path = f'{current_dir}/data/raw/trip-data_{type}_{year}-{month}.parquet'
+                 
+                dataframe.iloc[:100000].to_excel(f"{dataframe_path}", index=False)
+                logging.info(msg=f'Extraction well-done for {type} with time {year}-{month}')
+                logging.info(msg=f'Data with {dataframe_path.info()} rows')              
+                
+                return dataframe
+            else:
+                print(f'Fail to obtain data: {request.status_code}')
+                return None
+        except Exception as e:
+            logging.error(msg=f'Error during extraction: {str(object=e)}')
             return None
-    
-        #ToDo
-            #Add a Sample of the extrated Raw Data
-            #Add 
+
         
-    def transform(self, df: pandas.DataFrame):
+    def transform(self, df: pandas.DataFrame) -> None:
         pass
     
-    def load(self, df: pandas.DataFrame, destination: str):
+    def load(self, df: pandas.DataFrame, destination: str)-> None:
         pass
     
 
